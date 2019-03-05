0d4cc4e4a872eb7cdbb4c9a772256e27df52e88a
jomy <jomy.tester@gmail.com>
2019-03-05 12:48:18 +0530

    Alert task completed

--- com.demoAutomationsite/src/test/java/com/demoAutomationsite/pages/com_ADS_alerts_pages.java
+++ com.demoAutomationsite/src/test/java/com/demoAutomationsite/pages/com_ADS_alerts_pages.java
@@ -12,23 +12,34 @@ import org.testng.Assert;
 public class com_ADS_alerts_pages {
 
 	WebDriver driver;
+	Alert globalAlertswitch;
 
 	@FindBy(xpath = "//*[@id=\"header\"]/nav/div/div[2]/ul/li[4]/a")
 	WebElement switchto_click;
+
 	@FindBy(xpath = "//a[contains(text(),'Alerts')]")
 	WebElement switchto_Alert;
+
 	@FindBy(id = "OKTab")
 	WebElement AlertWithOk;
+
 	@FindBy(xpath = "//a[contains(text(),'Alert with OK & Cancel')]")
 	WebElement Alertokcancel_tab;
+
 	@FindBy(id = "CancelTab")
 	WebElement Alertokcancel_click;
 
 	@FindBy(id = "demo")
 	WebElement message_okCancel;
-	
-	@FindBy(xpath="//a[contains(text(),'Alert with Textbox')]") WebElement alertTextbox_tab;
-	@FindBy(xpath ="//div[@class='Textbox']//button") WebElement Alertwithtext_click;
+
+	@FindBy(xpath = "//a[contains(text(),'Alert with Textbox')]")
+	WebElement alertTextbox_tab;
+
+	@FindBy(xpath = "//button[@onclick='promptbox()']")
+	WebElement Alertwithtext_click;
+
+	@FindBy(id = "demo1")
+	WebElement message_promptbox;
 
 	public com_ADS_alerts_pages(WebDriver driver) {
 
@@ -44,40 +55,65 @@ public class com_ADS_alerts_pages {
 		moveTo_alert.moveToElement(switchto_Alert).click().build().perform();
 		AlertWithOk.click();
 
-		Alert alert_ok = driver.switchTo().alert();
+		globalAlertswitch = driver.switchTo().alert();
 
-		alert_ok.accept();
-
-		
+		globalAlertswitch.accept();
 
 	}
-	
-	public void alert_okCancel(){
-		
+
+	public void alert_okCancel() {
+
 		Alertokcancel_tab.click();
 
 		Alertokcancel_click.click();
 
-		Alert Okcancel_Alert_ok = driver.switchTo().alert();
-		Okcancel_Alert_ok.dismiss();
+		globalAlertswitch = driver.switchTo().alert();
+		globalAlertswitch.dismiss();
+
 		String actual = message_okCancel.getText();
 		String Expected = "You Pressed Cancel";
-
 		Assert.assertEquals(Expected, actual);
 
 		Alertokcancel_click.click();
-		Okcancel_Alert_ok.accept();
+		globalAlertswitch.accept();
 
 		String actual1 = message_okCancel.getText();
 		String Expected1 = "You pressed Ok";
 
 		Assert.assertEquals(Expected1, actual1);
-		
+
 	}
-	
+
 	public void alert_textbox() {
-		
+
 		alertTextbox_tab.click();
+
 		Alertwithtext_click.click();
+
+		globalAlertswitch = driver.switchTo().alert();
+
+		globalAlertswitch.dismiss();
+
+		Alertwithtext_click.click();
+
+		globalAlertswitch.sendKeys("jomy george");
+
+		
+		String expected = "jomy george";
+
+		globalAlertswitch.accept();
+
+		String actual = message_promptbox.getText();
+		
+		System.out.println(actual);
+
+		if (actual.contains(expected)) {
+
+			System.out.println("Condition passed");
+		} else {
+
+			System.out.println("Condition Failed");
+		}
+
 	}
 }
--- com.demoAutomationsite/src/test/java/com/demoAutomationsite/testcases/ADS_register.java
+++ com.demoAutomationsite/src/test/java/com/demoAutomationsite/testcases/ADS_register.java
@@ -9,8 +9,8 @@ public class ADS_register extends BaseClass_ADS {
 	@Test
 	public void register_ads() throws Exception {
 
-		register_page.Register(dr.ReadDat(2, 0), dr.ReadDat(2, 1), dr.ReadDat(2, 2));
-		Tables.Table_view();
+		//register_page.Register(dr.ReadDat(2, 0), dr.ReadDat(2, 1), dr.ReadDat(2, 2));
+		//Tables.Table_view();
 		alert_handle.alert_ok();
 		alert_handle.alert_okCancel();
 		alert_handle.alert_textbox();
